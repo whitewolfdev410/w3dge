8097b37373a23858945037bb5884e87ed928a7f7
whitewolfdev410 <whitewolf96410@gmail.com>
2024-09-16 08:40:51 -0700

    fix space between header and title

--- src/components/tabs/tabComponent.tsx
+++ src/components/tabs/tabComponent.tsx
@@ -9,20 +9,20 @@ const TabComponent = () => {
 
   return (
     <div>
-      <p className="font-bold font-GBold text-[1rem] text-white mt-14">Ask a question</p>
-      <p className="font-normal font-GRegular text-[1rem] text-white mt-14">
+      <p className="font-bold font-GBold text-[1rem] text-white mt-6">Ask a question</p>
+      <p className="font-normal font-GRegular text-[1rem] text-white mt-4">
         We know that 1 day = 24 hours so , 7 multiply by 24 7 days = 7 x 24 =
         168 Hence there are 168 hours in 7 days.
       </p>
       {isExpanded && (
         <>
-          <p className="font-bold font-GBold text-[1rem] text-white mt-14">Ask a question</p>
-          <p className="font-normal font-GRegular text-[1rem] text-white mt-14">
+          <p className="font-bold font-GBold text-[1rem] text-white mt-6">Ask a question</p>
+          <p className="font-normal font-GRegular text-[1rem] text-white mt-4">
             We know that 1 day = 24 hours so , 7 multiply by 24 7 days = 7 x 24 =
             168 Hence there are 168 hours in 7 days.
           </p>
-          <p className="font-bold font-GBold text-[1rem] text-white mt-14">Ask a question</p>
-          <p className="font-normal font-GRegular text-[1rem] text-white mt-14">
+          <p className="font-bold font-GBold text-[1rem] text-white mt-6">Ask a question</p>
+          <p className="font-normal font-GRegular text-[1rem] text-white mt-4">
             We know that 1 day = 24 hours so , 7 multiply by 24 7 days = 7 x 24 =
             168 Hence there are 168 hours in 7 days.
           </p>
