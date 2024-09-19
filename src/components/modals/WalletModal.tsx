c95fd01c962b541606c64c4317428f0dbe423c13
whitewolfdev410 <whitewolf96410@gmail.com>
2024-09-19 01:32:24 -0700

    fix build issue

--- src/components/modals/WalletModal.tsx
+++ src/components/modals/WalletModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { Connector, useConnect, useAccount } from 'wagmi';
 import { BrowserProvider } from 'ethers';
 
